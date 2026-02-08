b8ceb9dc6dcbbad829b69786abe1694775a2c8ea
bernardonascimento09-hue <bernardonascimento09@gmail.com>
2026-02-08 10:19:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {
   // Firebase Realtime Database URL (example: https://your-project-default-rtdb.firebaseio.com)
-  firebaseDatabaseUrl: "https://vac-calc-default-rtdb.firebaseio.com/"
+  firebaseDatabaseUrl: "https://vac-calc-default-rtdb.firebaseio.com"
 };
